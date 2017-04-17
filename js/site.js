b7e868cc3dceae5b46f16fda0eab9473bcd94b30
Izaaz Kothawala <ikothawa@hawk.iit.edu>
2017-04-17 16:15:06 -0500

    fixed some styling and adding effects for webpage background

--- js/site.js
+++ js/site.js
@@ -1,10 +1,13 @@
+//Teel jQuery to give up the dollar sign
 $.noConflict();
 
+//jQuery 3.x-style ready event and locally scoped $
 jQuery(function($){
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
-  
-  //testing username and password 
+
+//js to test username and password
+var BackgroundColor="RED"; 
 var error = 'error';
 var showError = false;
 window.currentUser = "";
@@ -17,4 +20,60 @@ $('#un').on('keyup focus blur', function(){
 $('#pass').on('keyup focus blur', function(){
     window.currentPassword = $(this).val();
 });
+
+ $('#form').on('submit', function(event) {
+ 	event.preventDefault();
+   if (currentUser=="admin" &&  currentPassword=="admin") {
+    var newURL = window.location= "class-schedule/index.html";
+    $('#form')[0].reset();
+    window.location.href = (newURL);
+
+    document.body.style.backgroundColor=BackgroundColor //changing bg color
+      }
+   else{
+      if (!showError) {
+         $('#list').append($('<li></li>', { class: error, text : 'Username/password combination not found'}));
+         showError=true;
+      }
+}
+
+});
+
+$('#logout').click(function(event){
+alert('You have been succesfully logged out.');
+   $('#form')[0].reset();
+   window.location = "../index.html";
+   event.preventDefault();
+});
+
+
+$('.fall2016').hide();
+$('.spring2017').hide();
+$('.fall2017').hide();
+$( "#back").hide();
+
+$('#f16').click(function(){
+  $( ".fall2016").show();
+  $( ".term").hide();
+  $( "#back").show();
+});
+
+$('#s17').click(function(){
+  $( ".spring2017").show();
+  $( ".term").hide();
+  $( "#back").show();
+});
+
+$('#f17').click(function(){
+  $( ".fall2017").show();
+  $( ".term").hide();
+  $( "#back").show();
+});
+
+$('#back').click(function(){
+  $( ".term").show();
+  $( "#back").hide();
+  $( "table").hide();
+});
+
 });
