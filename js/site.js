aef0dcfa7ef7b55a7520950dff342486a0c5b1ea
fwahid <fwahid@hawk.iit.edu>
2017-04-17 10:20:38 -0500

    added test js for login

--- js/site.js
+++ js/site.js
@@ -1,7 +1,20 @@
 $.noConflict();
 
-//jQuery 3.x-style ready event and locally scoped $
 jQuery(function($){
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
+  
+  //testing username and password 
+var error = 'error';
+var showError = false;
+window.currentUser = "";
+window.currentPassword = "";
+
+$('#un').on('keyup focus blur', function(){
+    window.currentUser = $(this).val();
+});
+
+$('#pass').on('keyup focus blur', function(){
+    window.currentPassword = $(this).val();
+});
 });
