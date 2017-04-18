55d5829b521266a3c6cf0861cd574d55870eaa3a
Izaaz Kothawala <ikothawa@hawk.iit.edu>
2017-04-17 21:01:51 -0500

    edited JS file for add drop function

--- js/site.js
+++ js/site.js
@@ -39,6 +39,25 @@ $('#pass').on('keyup focus blur', function(){
 
 });
 
+function submitRowAsForm(idRow) {
+  form = document.createElement("form"); // CREATE A NEW FORM TO DUMP ELEMENTS INTO FOR SUBMISSION
+  form.method = "post"; // CHOOSE FORM SUBMISSION METHOD, "GET" OR "POST"
+  form.action = ""; // TELL THE FORM WHAT PAGE TO SUBMIT TO
+  $("#"+idRow+" td").children().each(function() { // GRAB ALL CHILD ELEMENTS OF <TD>'S IN THE ROW IDENTIFIED BY idRow, CLONE THEM, AND DUMP THEM IN OUR FORM
+        if(this.type.substring(0,6) == "select") { // JQUERY DOESN'T CLONE <SELECT> ELEMENTS PROPERLY, SO HANDLE THAT
+            input = document.createElement("input"); // CREATE AN ELEMENT TO COPY VALUES TO
+            input.type = "hidden";
+            input.name = this.name; // GIVE ELEMENT SAME NAME AS THE <SELECT>
+            input.value = this.value; // ASSIGN THE VALUE FROM THE <SELECT>
+            form.appendChild(input);
+        } else { // IF IT'S NOT A SELECT ELEMENT, JUST CLONE IT.
+            $(this).clone().appendTo(form);
+        }
+
+    });
+  form.submit(); // NOW SUBMIT THE FORM THAT WE'VE JUST CREATED AND POPULATED
+}
+
 $('#logout').click(function(event){
 alert('You have been succesfully logged out.');
    $('#form')[0].reset();
@@ -77,3 +96,5 @@ $('#back').click(function(){
 });
 
 });
+
+
